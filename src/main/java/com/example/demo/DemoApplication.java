a8653dcbfc80d97ff2832c87befcc3b078eaed90
edwin-737 <ed.wongso2003@gmail.com>
2023-02-11 19:27:41 +0800

    testing post route

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @SpringBootApplication
 @RestController
@@ -16,5 +20,12 @@ public class DemoApplication {
 	@RequestMapping("/")
 	String sayHello() {
 		return "Hello World!";
+	}
+	@PostMapping("/request")
+	public ResponseEntity postController(
+			@RequestBody LoginForm loginForm) {
+		System.out.println(loginForm.username);
+		System.out.println(loginForm.password);
+		return ResponseEntity.ok(HttpStatus.OK);
 	}
 }
---
+++ src/main/java/com/example/demo/LoginForm.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class LoginForm {
+    public String username;
+    public String password;
+    // ...
+
+}
